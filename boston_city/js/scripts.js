48721c480d5a7e9a76b113ac2404cc992222284b
Diana Putnam <dianaa.putnam@gmail.com>
2022-11-14 23:54:43 -0700

    boston city page added

---
+++ boston_city/js/scripts.js
@@ -0,0 +1,31 @@
+
+
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () =>{
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+});
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000, 
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Enable autoplay 
+    autoplay: {
+      delay: 5000, 
+    },
+  
+  });
